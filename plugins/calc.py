f1ca352b077ae039412fe5fa16c636439d92546a
Sydney Erickson <sydneyerickson@icloud.com>
2017-01-23 14:05:53 -0800

    Removing debugging lines and adding some "error checking"

--- plugins/calc.py
+++ plugins/calc.py
@@ -2,7 +2,9 @@ def init(plugin):
     return [plugin, ["calc"]]
 
 def on_command(command, text):
-    print(command)
     if(command == "calc"):
-        result = str(eval(text))
-        return "", result
+        try:
+            result = str(eval(text))
+            return "", result
+        except:
+            return "", "Input was not a valid math equation :("
