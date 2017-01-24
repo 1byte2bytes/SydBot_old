40c15ffe4ebb84f8af942830dcedcde24b63e03b
Sydney Erickson <sydneyerickson@icloud.com>
2017-01-24 10:09:10 -0800

    Adding copyright notices to nullify library

--- libraries/nullify.py
+++ libraries/nullify.py
@@ -1,3 +1,30 @@
+#Original code from https://github.com/corpnewt/CorpBot.py/tree/a3f0f419192dbae17f537a02dc377a41acf90757 is under the following license
+#
+#MIT License
+#
+#Copyright (c) 2016-2017 CorpNewt
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#===================================================
+#
+#SydBot is Copyright (c) Sydney 2017
+#
+#This is free and unencumbered software released into the public domain.
+#
+#Anyone is free to copy, modify, publish, use, compile, sell, or
+#distribute this software, either in source code form or as a compiled
+#binary, for any purpose, commercial or non-commercial, and by any
+#means.
+#
+#===================================================
+#
+
 def clean(string):
     # A helper script to strip out @here and @everyone mentions
     zerospace = "​"
