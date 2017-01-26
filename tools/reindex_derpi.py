9a64f45f4b32beb2969c2c6e5b5a10e890b82b7c
Sydney Erickson <sydneyerickson@icloud.com>
2017-01-26 14:04:24 -0800

    Significant speedup and error handling in Derpibooru indec script

--- tools/reindex_derpi.py
+++ tools/reindex_derpi.py
@@ -4,18 +4,29 @@ import json
 from tqdm import tqdm
 
 db = dataset.connect('sqlite:///../data/derpi.db')
+#db = dataset.connect('sqlite:///:memory:')
+
+table = db['images']
 
 image_num = 1
 image_set = glob.glob("../data/derpi_raw/*.json")
 image_total = len(image_set)
+
+no_tags = []
+invalid = []
 for image in tqdm(image_set):
-    #print(str(image_num) + " of " + str(image_total))
     image_id = image.rsplit("/", 1)[1]
-    #print(image_id)
     with open(image) as data_file:
         data = json.load(data_file)
-        table = db['image_{}'.format(image_id[:-5])]
-        for item in data:
-            #print(item)
-            #print(data[item])
-            table.insert(dict(name='{}'.format(item), data='{}'.format(data[item])))
+        try:
+            table.insert(dict(name=data["id"], imtags=data["tags"], image_url=data["representations"]["full"]))
+        except Exception as e:
+            try:
+                table.insert(dict(name=data["id"], imtags='', image_url=data["representations"]["full"]))
+                no_tags.append(data["id"])
+            except Exception as e:
+                invalid.append(data["id"])
+    db.commit()
+
+print("\r\n" + str(len(no_tags)) + " of " + str(image_total) + " images have no tags.")
+print("" + str(len(invalid)) + " of " + str(image_total) + " images are damaged.")
