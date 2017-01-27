5a4e8f538d6bbdd3279da933328f0f1d09654e98
Sydney Erickson <sydneyerickson@icloud.com>
2017-01-26 19:36:27 -0800

    Task to get newest Derpibooru images

---
+++ tasks/derpi_get_json_newest.py
@@ -0,0 +1,20 @@
+import urllib.request
+import json
+import os.path
+
+def do_task():
+    urllib.request.urlretrieve("https://derpibooru.org/images.json?constraint=created&order=d", "./data/derpi_latest.json")
+    images_to_grab = []
+    with open("./data/derpi_latest.json") as data_file:
+        data = json.load(data_file)
+        for image in data["images"]:
+            image_id = image["id"]
+            images_to_grab.append(image_id)
+    for image in images_to_grab:
+        if os.path.isfile("./data/derpi_raw/{}.json".format(image)):
+            print("[DEBUG  ] Derpibooru JSON file {}.json is already downloaded".format(image))
+            pass
+        else:
+            print("[DEBUG  ] Downloading Derpibooru JSON file {}.json".format(image))
+            urllib.request.urlretrieve("https://derpibooru.org/{}.json".format(image), "./data/derpi_raw/{}.json".format(image))
+    #print(images_to_grab)
